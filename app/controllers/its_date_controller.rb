f8a4f914120102c0b87cd76ab4f5c426df7ebfc5
Ardra T Syam <tsyamardra@gmail.com>
2026-07-24 11:39:24 +0530

    Use Resend API for emails

--- app/controllers/its_date_controller.rb
+++ app/controllers/its_date_controller.rb
@@ -1,10 +1,19 @@
 class ItsDateController < ApplicationController
   def index
-    DateMailer.date_summary(
-      session[:date],
-      session[:time],
-      session[:perfect_date],
-      session[:gift]
-    ).deliver_later
+    resend = Resend::Client.new(api_key: ENV["RESEND_API_KEY"])
+
+    resend.emails.send(
+      from: "onboarding@resend.dev",
+      to: "tsyamardra@gmail.com",
+      subject: "New Date Plan",
+      html: <<~HTML
+        <h2>💖 New Date Plan</h2>
+
+        <p><strong>Date:</strong> #{session[:date]}</p>
+        <p><strong>Time:</strong> #{session[:time]}</p>
+        <p><strong>Perfect Date:</strong> #{session[:perfect_date]}</p>
+        <p><strong>Gift:</strong> #{session[:gift]}</p>
+      HTML
+    )
   end
 end
